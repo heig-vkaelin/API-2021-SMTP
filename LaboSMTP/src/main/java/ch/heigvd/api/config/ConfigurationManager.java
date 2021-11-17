f208cc8d242994610649d8600c189466b32fd301
Valentin Kaelin <kalanehd@gmail.com>
2021-11-17 12:01:03 +0100

    docs: add stackoverflow link2

--- LaboSMTP/src/main/java/ch/heigvd/api/config/ConfigurationManager.java
+++ LaboSMTP/src/main/java/ch/heigvd/api/config/ConfigurationManager.java
@@ -14,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ConfigurationManager implements IConfigurationManager {
+    // Regex récupérée sur https://stackoverflow.com/a/8204716/9188650
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     
