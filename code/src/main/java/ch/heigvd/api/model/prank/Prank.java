5ecc19c953b09cd1d2e8e7ad9de15a00544cd31f
Valentin Kaelin <kalanehd@gmail.com>
2021-12-04 17:28:05 +0100

    refactor: store group in prank instead of list

--- code/src/main/java/ch/heigvd/api/model/prank/Prank.java
+++ code/src/main/java/ch/heigvd/api/model/prank/Prank.java
@@ -6,42 +6,37 @@ import ch.heigvd.api.model.mail.Message;
 import ch.heigvd.api.model.mail.Person;
 import ch.heigvd.api.model.mail.Group;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Prank {
-    private final Person sender;
-    private final List<Person> people;
+    private final Group group;
     private final Person copy;
     private final Message message;
     
-    public Prank(Group people, Message message, Person bcc) {
-        if (people.getMembers().size() < IConfigurationManager.MIN_SIZE_GROUP) {
+    public Prank(Group group, Message message, Person bcc) {
+        if (group.getMembers().size() < IConfigurationManager.MIN_SIZE_GROUP) {
             throw new RuntimeException("The group hasn't enough members to create " +
                     "a prank");
         }
         
+        this.group = group;
         this.message = message;
-        this.people = new ArrayList<>();
-        // 1ère personne de la liste people: le sender
-        this.sender = people.getMembers().get(0);
-        
-        for (int i = 1; i < people.getMembers().size(); i++) {
-            this.people.add(people.getMembers().get(i));
-        }
         this.copy = bcc;
     }
     
     public Mail generateMail() {
-        List<String> to = people.stream()
+        // 1ère personne de la liste: le sender
+        String sender = group.getMembers().get(0).getMailAddress();
+        List<String> to = group.getMembers().stream()
+                .skip(1)
                 .map(Person::getMailAddress)
                 .collect(Collectors.toList());
         
         return new Mail(
                 message.getSubject(),
                 message.getContent(),
-                sender.getMailAddress(),
+                sender,
                 to,
                 copy.getMailAddress()
         );
